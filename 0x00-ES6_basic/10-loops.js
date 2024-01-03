5f63f77daa608be02610a101364c6fccf3ad9e5f
Noransaber <noransaber685@gmail.com>
2024-01-03 18:56:01 +0200

    Removing letter i

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const idx of array) {
-    arrayEnd.push(`${appendString}${idx}`);
+    newArray.push(`${appendString}${idx}`);
   }
 
   return newArray;
