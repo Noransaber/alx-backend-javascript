db24b3968d53565bd955da77564677c459bc0ef5
Noransaber <noransaber685@gmail.com>
2024-01-08 14:37:59 +0200

    Solving two problems

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(arr) {
+  const keyToAccess = 'id';
+  if (Array.isArray(arr)) {
+    const valuesArray = arr.map((ele) => ele[keyToAccess]);
+    return valuesArray;
+  }
+  return [];
+}
