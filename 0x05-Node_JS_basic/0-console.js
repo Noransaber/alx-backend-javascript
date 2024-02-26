39b31c9efe0606ccea872b2e533e7fc72cecc030
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-26 17:59:16 +0200

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,10 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
+ * @author Noran Saber <https://github.com/Noransaber>
  */
 const displayMessage = (msg) => {
-    console.log(msg);
-  };
-  
-  module.exports = displayMessage;
+  console.log(msg);
+};
+
+module.exports = displayMessage;
