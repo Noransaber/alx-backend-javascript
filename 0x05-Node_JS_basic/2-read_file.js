c03d318694d1d684ed1eed55ed11b76166c55d74
Noran S Abdelfattah <noransaber685@gmail.com>
2024-02-26 18:53:03 +0200

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Noran <https://github.com/Noransaber>
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
